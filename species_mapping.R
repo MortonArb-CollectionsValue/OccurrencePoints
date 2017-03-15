4e22dbcb12d2b7ed2174dd50edd4101f938b7386
Drew Duckett <duckettdj@g.cofc.edu>
2017-03-15 14:40:27 -0500

    Create maps for species range and accession locations
    Initial commit of a code to download  rangemaps for a list of species
    and plot where accessions are from and where accessions are currently
    located. Currently code formats name for web address to download
    shapefiles

---
+++ species_mapping.R
@@ -0,0 +1,16 @@
+https://esp.cr.usgs.gov/data/little/querdoug.zip
+
+gen1 <- Oak_Collection_Species$Genus[1] #get genus name from table
+spec1 <- Oak_Collection_Species$Species[1] #get species name from table
+
+gen2 <- tolower(unlist(strsplit(gen1, ""))[1:4]) #split genus letters and lower case
+spec2 <- tolower(unlist(strsplit(spec1, ""))[1:4]) #split species letters and lower case
+
+gen3 <- paste(gen2, collapse = "") #convert characters back to string
+spec3 <- paste(spec2, collapse = "") #convert characters back to string
+
+genspec <- str_c(gen3, spec3) #combine genus and species strings
+
+webadd <- "https://esp.cr.usgs.gov/data/little/" #create string for beginning of web address
+zip <- ".zip" #create string for end of web address
+webadd2 <- str_c(webadd, genspec, zip) #combine all strings to make complete web address
