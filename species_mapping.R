1a31f121567d6bf177f18b45076413ae2c125064
Drew Duckett <duckettdj@g.cofc.edu>
2017-03-15 16:06:36 -0500

    Fixed bug and added code to extract shapefile
    The previous code for downloading the file did not work correctly, but
    this has been fixed. Also, code now extracts shapefile from zip file,
    but not able to read shapefile into R yet

--- species_mapping.R
+++ species_mapping.R
@@ -1,7 +1,10 @@
+library(stringr)
+library(rgdal)
+
 https://esp.cr.usgs.gov/data/little/querdoug.zip
 
-gen1 <- Oak_Collection_Species$Genus[1] #get genus name from table
-spec1 <- Oak_Collection_Species$Species[1] #get species name from table
+gen1 <- Oak_Collection_Species$Genus[2] #get genus name from table
+spec1 <- Oak_Collection_Species$Species[2] #get species name from table
 
 gen2 <- tolower(unlist(strsplit(gen1, ""))[1:4]) #split genus letters and lower case
 spec2 <- tolower(unlist(strsplit(spec1, ""))[1:4]) #split species letters and lower case
@@ -17,4 +20,8 @@ webadd2 <- str_c(webadd, genspec, zip) #combine all strings to make complete web
 
 wd1 <- getwd() #get working directory
 wd2 <- str_c(wd1, genspec, sep = "/") #format working directory to include filename 
-curl_download(webadd2, wd2) #download file to working directory with filename
+wd3 <- str_c(wd2, zip) #add .zip extension
+download.file(webadd2, wd3) #download zip file from web
+unzip("queralba.zip", "queralba.shp") #extract shapefile from zip file
+shapefile1 <- readOGR(dsn = shape_name) #DOES NOT WORK
+shape_name = "~/Forest_Ecology/IMLS_Mapping/queralba.shp"
