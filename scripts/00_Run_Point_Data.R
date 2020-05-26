cbad55e2d1efc2dd524315a7156e7e93e8c2b641
Emily Beckman <ebeckman@mortonarb.org>
2020-05-26 10:01:50 -0500

    Update description

--- scripts/00_Run_Point_Data.R
+++ scripts/00_Run_Point_Data.R
@@ -1,11 +1,18 @@
-### 00_Run_Point_Data.R
+# STILL WORKING -- NOT READY YET
 
-## This is the file to run all of the scripts for the point data aggregation and cleaning. This also is an outline for what is happening and why.
+################################################################################
+
+### Authors: Emily Beckman & Shannon Still ### Date: 05/21/2020
+
+### DESCRIPTION:
+  # This is the file to run all of the scripts for the point data aggregation
+  #   and cleaning. This also is an outline for what is happening and why.
 
 ##############################
 ##############################
 ## Source the working directory file to set up installation/run
 ###############
+#setwd("/Users/emily/Documents/GitHub/IMLS_CollectionsValue")
 source('scripts/set_workingdirectory.R')
 
 ##############################
@@ -28,27 +35,27 @@ source('scripts/2_get_raw_occurrence_points.R')
 # ###############
 # ## Run the retrieve raw EOs and then sort, clean, and filter
 # source('scripts/3_compile_raw_occurrence_points.R')
-# 
+#
 # ##############################
 # ##############################
 # ## 03_compile_raw_occurrence_points.R ##
 # ###############
 # ## Run the retrieve raw EOs and then sort, clean, and filter
 # source('scripts/03_compile_raw_occurrence_points.R')
-# 
+#
 # ##############################
 # ##############################
 # ## 03_compile_raw_occurrence_points.R ##
 # ###############
 # ## Run the retrieve raw EOs and then sort, clean, and filter
 # source('scripts/03_compile_raw_occurrence_points.R')
-# 
+#
 # ##############################
 # ##############################
 # ## 03_compile_raw_occurrence_points.R ##
 # ###############
 # ## Run the retrieve raw EOs and then sort, clean, and filter
 # source('scripts/03_compile_raw_occurrence_points.R')
-# 
-# 
-# 
+#
+#
+#
