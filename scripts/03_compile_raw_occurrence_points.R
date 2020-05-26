50745072c4c0f6f81959296e281912ddd73802b0
Emily Beckman <ebeckman@mortonarb.org>
2020-05-26 10:04:46 -0500

    fixing up; added check to see if points are on land

--- scripts/03_compile_raw_occurrence_points.R
+++ scripts/03_compile_raw_occurrence_points.R
@@ -1,4 +1,6 @@
-### Author: Emily Beckman & Shannon Still ###  Date: 05/05/2020                                |
+################################################################################
+
+### Authors: Emily Beckman & Shannon Still ### Date: 05/05/2020
 
 ### DESCRIPTION:
   # This script compiles data downloaded in 2_get_raw_occurrence_points.R,
@@ -19,42 +21,36 @@
     # folder (raw_split_by_sp) with CSV of raw occurrence data for each target
     #   species (e.g., Malus_angustifolia.csv)
 
-#################
-### LIBRARIES ###
-#################
+
+################################################################################
+# Load packages
+################################################################################
 
 rm(list=ls())
-my.packages <- c('plyr', 'tidyverse', 'housingData', 'data.table', 'textclean', 'CoordinateCleaner')
+my.packages <- c('plyr', 'tidyverse', 'housingData', 'data.table', 'textclean',
+  'CoordinateCleaner', 'maps', 'rnaturalearth', 'rnaturalearthdata')
 # install.packages (my.packages) #Turn on to install current versions
 lapply(my.packages, require, character.only=TRUE)
 rm(my.packages)
 
-#######################################
-# run code to set your working directory and project folders based upon computer using
-#      skip this if preferred, but then need to set your working directory and input/output folders manually
-      # If not using working directory script, be sure to create data_in path
-####################################################################################
+
+### could remove once we get "00" script up and running
+#source("./Documents/GitHub/IMLS_CollectionsValue/scripts/set_workingdirectory.R")
 source('scripts/set_workingdirectory.R')
 
-# setwd("./../..")
- setwd("/Volumes/GoogleDrive/Shared drives/IMLS MFA/insitu_occurrence_points")
- data_in <- "/Volumes/GoogleDrive/Shared drives/IMLS MFA"
-
 ################################################################################
-# A) Read in data and stack
+# A) Read in raw data and stack
 ################################################################################
 
 # read in raw datasets
-  ## using file.path, I have listed the files in the directory "raw_occurrence_point_data"
-  ## data_in is an object defined in the script 'set_workingdirectory.R'
-file_list <- list.files(file.path(data_in, 'insitu_occurrence_points', "raw_occurrence_point_data"),
-                        pattern = ".csv", full.names = T)
-file_dfs <- lapply(file_list, read.csv, header = T, na.strings=c("","NA"),
-                   colClasses="character")
+file_list <- list.files(file.path(imls.raw, "raw_datasets"), pattern = ".csv",
+  full.names = T)
+file_dfs <- lapply(file_list, read.csv, header = T, na.strings = c("","NA"),
+  colClasses = "character")
 length(file_dfs) #6
 
 # stack all datasets using rbind.fill, which keeps non-matching columns
-#   and fills with NA; 'Reduce' iterates through list and merges with previous
+#   and fills with NA; 'Reduce' iterates through list and merges with previous.
 # this may take a few minutes if you have lots of data
 all_data_raw <- Reduce(rbind.fill, file_dfs)
   nrow(all_data_raw) #8085017
@@ -66,16 +62,14 @@ all_data_raw <- Reduce(rbind.fill, file_dfs)
 ################################################################################
 
 # read in target taxa list
-taxon_list <- read.csv(file.path(data_in, 'insitu_occurrence_points', "target_taxa_with_syn.csv"),
-                       header=TRUE, na.strings=c("","NA"), colClasses="character")
-  # taxon_list <- read.csv("target_taxa_with_syn.csv", header = T,
-  #       na.strings=c("","NA"), colClasses="character")
+taxon_list <- read.csv(file.path(imls.meta, "target_taxa_with_syn.csv"),
+  header = T, na.strings = c("","NA"), colClasses = "character")
 
 # full join to taxon list
 all_data_raw <- left_join(all_data_raw,taxon_list)
 # join again just by species name if no taxon match
 need_match <- all_data_raw[which(is.na(all_data_raw$list)),]
-  nrow(need_match) #124399
+  nrow(need_match) #124547
   # remove columns from first taxon name match
 need_match <- need_match[,1:(ncol(all_data_raw)-ncol(taxon_list)+1)]
   # rename column for matching
@@ -87,31 +81,36 @@ need_match <- left_join(need_match,taxon_list)
 matched <- all_data_raw[which(!is.na(all_data_raw$list)),]
 matched$taxon_name_full <- matched$taxon_name
 all_data <- rbind(matched,need_match)
-  table(all_data$list) # desiderata: 5924118 | synonym: 2102339
+  table(all_data$list) # desiderata: 5928806 | synonym: 2102904
 
 # check names that got excluded.....
 still_no_match <- all_data[which(is.na(all_data$list)),]
-  nrow(still_no_match) #624119
+  nrow(still_no_match) #58560
 table(still_no_match$database)
-sort(table(still_no_match$taxon_name))
+#sort(table(still_no_match$taxon_name))
 
 # keep only rows for target taxa
 all_data <- all_data[which(!is.na(all_data$list)),]
-  nrow(all_data) #8026457
+  nrow(all_data) #8031710
 
 ################################################################################
 # C) Standardize some key columns
-  ## this step could be down in script 2, if we want to take the time to go through it and add column header files
 ################################################################################
+
+## this section could potentially be moved to script 02
 
 # create localityDescription column
 all_data <- all_data %>% unite("localityDescription",
   c(locality,verbatimLocality,municipality,higherGeography,county,stateProvince,
-    country,countryCode),remove=F,sep=" | ") #na.rm=T,
-all_data$localityDescription <-
-  mgsub(all_data$localityDescription,c("NA "," NA"),"")
-all_data$localityDescription <-
-  gsub("| | | | | | |",NA,all_data$localityDescription,fixed=T)
+    country,countryCode), remove = F, sep = " | ")
+  # get rid of NAs but keep pipes, so you can split back into parts if desired
+all_data$localityDescription <- mgsub(all_data$localityDescription,
+  c("NA "," NA"), "")
+  # if no locality info at all, make it NA
+all_data$localityDescription <- gsub("| | | | | | |", NA,
+  all_data$localityDescription, fixed = T)
+  # check it
+head(unique(all_data$localityDescription))
 
 # check year column
 unique(all_data$year)
@@ -128,16 +127,14 @@ all_data$establishmentMeans[which(is.na(all_data$establishmentMeans))] <-
   # convert to numeric
 all_data$decimalLatitude <- as.numeric(all_data$decimalLatitude)
 all_data$decimalLongitude <- as.numeric(all_data$decimalLongitude)
-  # if coord is 0, set to NA
-all_data$decimalLatitude[which(all_data$decimalLatitude==0)] <- NA
-all_data$decimalLongitude[which(all_data$decimalLongitude==0)] <- NA
-  nrow(all_data[which(is.na(all_data$decimalLatitude) |
-    is.na(all_data$decimalLongitude)),]) #459896
+  # if coords are both 0, set to NA
+zero <- which(all_data$decimalLatitude == 0 & all_data$decimalLongitude == 0)
+all_data$decimalLatitude[zero] <- NA; all_data$decimalLongitude[zero] <- NA
   # flag non-numeric and not available coordinates and lat > 90, lat < -90,
   # lon > 180, and lon < -180
-coord_test <- cc_val(all_data,lon = "decimalLongitude",lat = "decimalLatitude",
+coord_test <- cc_val(all_data, lon = "decimalLongitude",lat = "decimalLatitude",
   value = "flagged", verbose = TRUE)
-  # try switching lat and long and check validity again
+  # try switching lat and long for invalid points and check validity again
 all_data[!coord_test,c("decimalLatitude","decimalLongitude")] <-
   all_data[!coord_test,c("decimalLongitude","decimalLatitude")]
 coord_test <- cc_val(all_data,lon = "decimalLongitude",lat = "decimalLatitude",
@@ -146,7 +143,7 @@ coord_test <- cc_val(all_data,lon = "decimalLongitude",lat = "decimalLatitude",
 all_data[!coord_test,c("decimalLatitude","decimalLongitude")] <- c(NA,NA)
 
 # set column order and remove a few unnecessary columns
-all_data <- all_data %>% select(species_name_acc,taxon_name,scientificName,
+all_data <- all_data %>% dplyr::select(species_name_acc,taxon_name,scientificName,
   taxonIdentificationNotes,database,year,basisOfRecord,establishmentMeans,
   decimalLatitude,decimalLongitude,coordinateUncertaintyInMeters,
   geolocationNotes,localityDescription,county,stateProvince,country,countryCode,
@@ -156,159 +153,144 @@ all_data <- all_data %>% select(species_name_acc,taxon_name,scientificName,
 # add unique ID column
 all_data$unique_id <- seq.int(nrow(all_data))
 
-# create subsets for locality-only points and lat-long points, then
-#   continue forward with just lat-long points
+# separate out points with locality description only (no lat-long)
 locality_pts <- all_data %>% filter(!is.na(localityDescription) &
   (is.na(decimalLatitude) | is.na(decimalLongitude))) %>%
   arrange(desc(year)) %>%
   distinct(species_name_acc,localityDescription,.keep_all=T)
-  nrow(locality_pts) #212931
-  write.csv(locality_pts,"need_geolocation.csv",row.names=F)
-geo_pts <- all_data %>% filter(!is.na(decimalLatitude) &
-  !is.na(decimalLongitude)) %>% select(-localityDescription)
-  nrow(geo_pts) #7566561
+nrow(locality_pts) #212901
+table(locality_pts$database)
+write.csv(locality_pts, file.path(imls.output, "need_geolocation.csv"),
+  row.names = F)
 
-##write out data to examine
-    ##can write to a trial data folder (set in set_workngdirectory.R script if you prefer)
-  write.csv(locality_pts, file.path(data_in, "need_geolocation.csv"), row.names=FALSE)
-  # write.csv(locality_pts,"need_geolocation.csv")
-  geo_pts <- all_data %>% filter(!is.na(decimalLatitude) &
-                                   !is.na(decimalLongitude))
-  nrow(geo_pts) #7562242
+# move forward with subset of points that do have lat and long
+geo_pts <- all_data %>%
+  filter(!is.na(decimalLatitude) &
+  !is.na(decimalLongitude)) %>%
+  dplyr::select(-localityDescription)
+    nrow(geo_pts) #7571820
 
-# ## writing a file out just to look it over
-# write.csv(geo_pts[1:1000,], file.path(trial_data, "are_geolocated.csv"), col.names=TRUE)
-  write.csv(all_data, file.path(trial_data, "all_data.csv"), col.names=TRUE, row.names=FALSE)
-  write.csv(geo_pts, file.path(trial_data, "are_geolocated.csv"), col.names=TRUE, row.names=FALSE)
+# check if points are in water, mark, and separate out as other file
+world_polygons <- ne_countries(type = 'countries', scale = 'medium')
+geo_pts$in_water <- is.na(map.where(world_polygons, geo_pts$decimalLongitude,
+  geo_pts$decimalLatitude))
+water_pts <- geo_pts %>%
+  filter(in_water) %>%
+  dplyr::select(-in_water)
+nrow(water_pts) #59030
+table(water_pts$database)
+write.csv(water_pts, file.path(imls.output, "not_on_land.csv"), row.names = F)
+
+# create final subset of geolocated points which are on land
+geo_pts <- geo_pts %>%
+  filter(!in_water) %>%
+  dplyr::select(-in_water)
+nrow(geo_pts) #7512790
+table(geo_pts$database)
+# can write a file just to look it over
+#write.csv(geo_pts, file.path(imls.local, "are_geolocated.csv"), row.names = F)
 
 ################################################################################
 # D) Remove duplicates
 ################################################################################
 
-# sort before removing duplicates
-  # by year
+## this section could potentially be moved to script 04
+## OTHER WAYS OF REMOVING DUPLICATES ARE ALSO POSSIBLE AND COULD MAKE MORE
+##    SENSE FOR A SPECIFIC WAY OF USIGN THE POINTS, including
+##    by grid cell, distance between points, etc...
+## The segement below removes spatial duplicates based on rounded lattitude
+##    and longitude. This is a simple fix that doesn't involved spatial data
+##    or complex spatial calculations.
+
+# sort before removing duplicates; can turn any of these on/off, or add others
+  # sort by basis of record
+geo_pts$basisOfRecord <- factor(geo_pts$basisOfRecord,
+  levels = c("PRESERVED_SPECIMEN","MATERIAL_SAMPLE","OBSERVATION",
+  "HUMAN_OBSERVATION","MACHINE_OBSERVATION","LITERATURE","FOSSIL_SPECIMEN",
+  "LIVING_SPECIMEN","UNKNOWN"))
+geo_pts <- geo_pts %>% arrange(basisOfRecord)
+  # sort by establishment means
+geo_pts$establishmentMeans <- factor(geo_pts$establishmentMeans,
+  levels = c("NATIVE","UNKNOWN","INTRODUCED","MANAGED","CUT","INVASIVE","DEAD"))
+geo_pts <- geo_pts %>% arrange(establishmentMeans)
+  # sort by coordiante uncertainty
+geo_pts$coordinateUncertaintyInMeters <-
+  as.numeric(geo_pts$coordinateUncertaintyInMeters)
+geo_pts <- geo_pts %>% arrange(geo_pts$coordinateUncertaintyInMeters)
+  # sort by year
 geo_pts <- geo_pts %>% arrange(desc(year))
-  # by dataset
+  # sort by dataset
 geo_pts$database <- factor(geo_pts$database,
-  levels = c("FIA","GBIF","US_Herbaria","iDigBio","BISON","BIEN"))
+  levels = c("FIA","GBIF","US_Herbaria","iDigBio","BISON","BIEN","Ex_situ"))
 geo_pts <- geo_pts %>% arrange(database)
-geo_pts$database <- as.character(geo_pts$database)
 
-# create rounded latitude and longitude columns for removing dups
-    ## this step may not be necessary due to other comparisons we are attempting
-geo_pts$lat_round <- round(geo_pts$decimalLatitude,digits=3)
-geo_pts$long_round <- round(geo_pts$decimalLongitude,digits=3)
+# create rounded latitude and longitude columns for removing duplicates
+#   number of digits can be changed based on how dense you want data
+geo_pts$lat_round <- round(geo_pts$decimalLatitude,digits=4)
+geo_pts$long_round <- round(geo_pts$decimalLongitude,digits=4)
 
-# remove duplicates
+# remove duplicates and create "all_source_databases" column, to capture
+#    databases from which duplicates were removed
+# can take a while to remove duplicates if there are lots a rows
 geo_pts2 <- geo_pts %>%
-  group_by(species_name_acc,lat_round,long_round,establishmentMeans) %>%
-  mutate(source_databases = paste(database,collapse = ',')) %>%
-  distinct(species_name_acc,lat_round,long_round,establishmentMeans,
-    .keep_all=T) %>%
+  group_by(species_name_acc,lat_round,long_round) %>%
+  #mutate(all_source_databases = paste(database,collapse = ',')) %>%
+  distinct(species_name_acc, lat_round, long_round, .keep_all=T) %>%
   ungroup()
-  # remove duplicates in source_databases column
-  s <- lapply(geo_pts2$source_databases, function(x)
-    unlist(strsplit(x,split=",")))
-  source_standard <- as.data.frame(unlist(lapply(s,function(x)
-    paste(sort(unique(x)),collapse=','))))
-  names(source_standard)[1] <- "source_databases"
-  geo_pts2 <- geo_pts2 %>% select(-source_databases) %>% cbind(source_standard)
+  # remove duplicates in all_source_databases column
+#parts <- lapply(geo_pts2$source_databases, function(x)
+#  unlist(strsplit(x,split=",")))
+#source_standard <- as.data.frame(unlist(lapply(parts,function(x)
+#  paste(sort(unique(x)),collapse=','))))
+#names(source_standard)[1] <- "all_source_databases"
+#geo_pts2 <- geo_pts2 %>%
+#  select(-all_source_databases) %>%
+#  cbind(source_standard)
 head(geo_pts2)
-nrow(geo_pts2) #1501081
-table(geo_pts2$source_databases)
+nrow(geo_pts2) #1367167
+#table(geo_pts2$all_source_databases)
 table(geo_pts2$database)
 
 # take a look at results
+  # lat-long records
 count_geo <- geo_pts2 %>% count(species_name_acc)
 count_geo <- setorder(count_geo,n)
 names(count_geo)[2] <- "num_latlong_records"
+  # water records
+count_water <- water_pts %>% count(species_name_acc)
+count_water <- setorder(count_water,n)
+names(count_water)[2] <- "num_water_records"
+  # locality-only records
 count_locality <- locality_pts %>% count(species_name_acc)
 count_locality <- setorder(count_locality,n)
 names(count_locality)[2] <- "num_locality_records"
-summary <- full_join(count_geo,count_locality)
-write.csv(summary,"occurrence_point_count_per_species_new.csv",row.names=F)
+  # make table of all categories
+files <- list(count_geo,count_water,count_locality)
+summary <- Reduce(full_join, files)
+head(summary)
+  # write file
+write.csv(summary, file.path(imls.output, "occurrence_point_count_per_sp.csv"),
+  row.names = F)
 
 ##save data out to a file so don't have to rerun
-save(all_data, taxon_list, s, geo_pts2, need_match, file=file.path(data_in, "EO_data.RData"))
-  rm(all_data_raw, file_dfs, geo_pts, locality_pts, matched, need_match, source_standard)
-#
-#
-#   head(all_data)
+#save(all_data, taxon_list, s, geo_pts2, need_match, file=file.path(data_in, "EO_data.RData"))
+#  rm(all_data_raw, file_dfs, geo_pts, locality_pts, matched, need_match, source_standard)
+#  head(all_data)
 
 ################################################################################
 # E) Split by species
 ################################################################################
-load(file.path(data_in, "EO_data.RData"))
 
-# split
+#load(file.path(data_in, "EO_data.RData"))
+
+# split lat-long points to create one CSV for each target species
 sp_split <- split(geo_pts2, as.factor(geo_pts2$species_name_acc))
 names(sp_split) <- gsub(" ","_",names(sp_split))
 
 # write files
-if(dir.exists(file.path(data_in, "raw_split_by_sp"))) print('directory already created') else
-  dir.create(file.path(data_in, "raw_split_by_sp"), recursive=TRUE)
-# dir.create(file.path(getwd(),"raw_split_by_sp"))
+if(dir.exists(file.path(imls.output, "split_by_sp")))
+  print('directory already created') else
+  dir.create(file.path(imls.output, "split_by_sp"), recursive = TRUE)
 lapply(seq_along(sp_split), function(i) write.csv(sp_split[[i]],
-  paste("raw_split_by_sp/",names(sp_split)[[i]],".csv",sep=""),row.names=F))
-
-
-
-################################################################################
-# VISUALIZE & EXPLORE POINTS
-## move to script #4 eventually?? or could be separate script like the current
-## "99" script. Really just for manual exporation, versus creating an output
-################################################################################
-
-library(leaflet); library(RColorBrewer)
-
-path.imls <- "/Volumes/GoogleDrive/Shared drives/IMLS MFA"
-path.pts <- file.path(path.imls, "insitu_occurrence_points/raw_split_by_sp")
-
-spp.test <- c("Quercus_georgiana", "Quercus_imbricaria", "Quercus_arkansana",
-  "Quercus_falcata", "Quercus_stellata", "Quercus_acutissima")
-
-dat.now <- read.csv(file.path(path.pts, paste0(spp.test[6],".csv")),
-  colClasses="character")
-
-dat.now$decimalLatitude <- as.numeric(dat.now$decimalLatitude)
-dat.now$decimalLongitude <- as.numeric(dat.now$decimalLongitude)
-dat.now$database <- factor(dat.now$database,
-  levels = c("FIA","GBIF","US_Herbaria","iDigBio","BISON","BIEN"))
-dat.now <- dat.now %>% arrange(desc(database))
-
-colors <- c("#db218e","#8a2fad","#2fb3b5","#4c9c24","#c29f21","#c2661f")
-palette <- colorFactor(palette=colors, levels=unique(dat.now$database))
-
-map.pts <- function(pts){
-	map <- leaflet() %>%
-		addProviderTiles("CartoDB.PositronNoLabels") %>%
-		addCircleMarkers(
-      data = pts,
-      lng = ~decimalLongitude,
-      lat = ~decimalLatitude,
-			popup = ~paste(
-        "Species name:",taxon_name_full,"(",list,")","<br/>",
-				"Database:",database,"<br/>",
-        "Dataset name:",datasetName,"<br/>",
-        "All source databases:",source_databases,"<br/>",
-        "Year:",year,"<br/>",
-				"Basis of record:",basisOfRecord,"<br/>",
-        "Establishment means:",establishmentMeans,"<br/>",
-        "Coordinate uncertainty:",coordinateUncertaintyInMeters,"<br/>",
-        "ID:",unique_id),
-			radius = 5,
-      fillOpacity = 0.6,
-      stroke = F,
-      color = ~palette(database)) %>%
-		addControl(
-      pts$species_name_acc[1],
-      position = "topright") %>%
-		addLegend(
-      pal = palette,
-      values = unique(dat.now$database),
-			title = "Key",
-      position = "bottomright",
-      opacity = 0.6)
-	return(map)
-}
-map.pts(dat.now)
+  file.path(imls.output, "split_by_sp", paste0(names(sp_split)[[i]], ".csv")),
+  row.names = F))
